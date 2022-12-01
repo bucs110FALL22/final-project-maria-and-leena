8642ebefa6990dd14b76a376a0cde9499cbcc274
Leena Ouafik <112440101+Leenaouafik@users.noreply.github.com>
2022-12-01 01:35:16 +0000

    snake color

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pygame
 import random
 import time
 import sys
+
 #import your controller
 pygame.init()
-dis=pygame.display.set_mode((400,300))
-pygame.display.set_caption('Snake game')
+dis = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Snake Game')
 #Create Screen
-surface = pygame.display.set_mode((400,300))
+surface = pygame.display.set_mode((400, 300))
 color = (95, 158, 160, 255)
 surface.fill(color)
 pygame.display.flip()
@@ -16,40 +17,78 @@ frame_size_x = 720
 frame_size_y = 480
 
 #Allow player to choose color of snake
-red:(255,0,0)
-yellow:(255,255,0)
-blue:(0,0,255)
-green:(0,128,0)
+red: (255, 0, 0)
+yellow: (255, 255, 0)
+blue: (0, 0, 255)
+green: (0, 128, 0)
 
-color_snake="Please input the color you would like your snake to be (red, yellow, blue green): "
+color_snake = "Please input the color you would like your snake to be (red, yellow, blue green): "
 
 #Create Snake
-snake=False
+snake = False
 while not snake:
     for event in pygame.event.get():
-      if event.type==pygame.QUIT:
-        snake=True
-    pygame.draw.rect(dis,color_snake,[200,150,10,10])
-    if color_snake=="red":
-      snake.color="red"
-    elif color_snake=="green":
-      snake.color="green"
-    elif color_snake=="blue":
-      snake.color="blue"
-    elif color_snake=="yellow":
-      snake.color="yellow"
+        if event.type == pygame.QUIT:
+            snake = True
+    pygame.draw.rect(dis, color_snake, [200, 150, 10, 10])
+    if color_snake == "red":
+        snake.color = "red"
+    elif color_snake == "green":
+        snake.color = "green"
+    elif color_snake == "blue":
+        snake.color = "blue"
+    elif color_snake == "yellow":
+        snake.color = "yellow"
     pygame.display.update()
 pygame.quit()
 quit()
- 
-    #Call your mainloop
-    
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
+#Call your mainloop
+
+###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
 # https://codefather.tech/blog/if-name-main-python/
 
-
 #Food (Random Coord)
-food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
+food_pos = [
+    random.randrange(1, (frame_size_x // 10)) * 10,
+    random.randrange(1, (frame_size_y // 10)) * 10
+]
 food_spawn = True
+
+
+#Buttons for Snake to move around
+class Snake_movement:
+
+    def __init__(self):
+        pass
+#Snake movement
+
+    def move(self, direction):
+        if direction == 'UP':
+            snake_pos_y: -10
+            snake_pos_x: 0
+        if direction == 'DOWN':
+            snake_pos_y: 10
+            snake_pos_x: 0
+        if direction == 'RIGHT':
+            snake_pos_y: 0
+            snake_pos_x: 10
+        if direction == 'LEFT':
+            snake_pos_y: 0
+            snake_pos_x: -10
+
+    def change_color(self, color_snake):
+        if color_snake == "red":
+            snake.color = "red"
+        elif color_snake == "green":
+            snake.color = "green"
+        elif color_snake == "blue":
+            snake.color = "blue"
+        elif color_snake == "yellow":
+            snake.color = "yellow"
+        else:
+            snake.color = "white"
+
+
+#Scoreboard
--- src/sample_controller.py
+++ src/sample_controller.py
@@ -1,8 +1,11 @@
+import src.snake
 
 class Controller:
   
   def __init__(self):
     #setup pygame data
+    self.state = "Game"
+    self.snake = snake.Snake()
     
   def mainloop(self):
     #select state loop
@@ -19,7 +22,21 @@ class Controller:
       #redraw
       
   def gameloop(self):
+    while self.state == "GAME":
       #event loop
+      for event in pygame.event.get(): 
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_DOWN: 
+            self.snake.move("DOWN")
+          if event.key == pygame.K_UP: 
+            self.snake.move('UP')
+          if event.key == pygame.K_RIGHT:
+            self.snake.move('RIGHT')
+          if event.key == pygame.K_LEFT: 
+            self.snake.move('LEFT')
+          if event.key == pgame.K_SPACE:
+            color = self.getcolorfromuser()
+            self.snake.change_color(color)
 
       #update data
 
